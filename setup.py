4dd148f6a22c6453ee427cd549528acc270dd914
Hamed J.I <hamed.ji@gmail.com>
2021-10-27 18:14:40 +0330

    Add github workflow

--- setup.py
+++ setup.py
@@ -1,21 +1,53 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import pathlib
 
-from os import path
+here = pathlib.Path(__file__).parent.resolve()
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='seq-uuid',
-    version='1.0.0',
-    packages=['seq_uuid'],
+    name='seq-uuid',  # Required
+    version='1.0.0',  # Required
     description='A library to generate sequential uuid based on RFC4122',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
     url='https://github.com/sebaloot/seq-uuid-py',
     author='Sebaloot',
-    license="MIT",
     keywords=['uuid', 'uuidv6', 'sequential uuid'],
-    classifiers=[]
+    package_dir={'': 'src'},  # Optional
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.6, <4',
+    install_requires=[],  # Optional
+    license="MIT",
+    extras_require={  # Optional
+        'dev': ['pytest', 'coverage']
+    },
+    package_data={  # Optional
+
+    },
 )
